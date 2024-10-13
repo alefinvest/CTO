9a816a887339ea6417c841d9bc7192e16d3d4b90
Oleksii Bondarenko <71653234+alexmazaltov@users.noreply.github.com>
2024-10-14 02:18:55 +0300

    CTO AI review feature implemented (#1)
    * Code of Conduct
    
    * Add telegram chatbot based on [grammyjs](https://github.com/grammyjs/grammy)
    
    * Implement AI review
    
    * Implement AI review -fix: repository name was wrong
    
    * cto_ai_bot adjustments for production build before deployment to vercel

--- dash.board/next.config.mjs
+++ dash.board/next.config.mjs
@@ -3,6 +3,10 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin("./src/core/i18n/i18n.ts");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        serverComponentsExternalPackages: ['grammy'],
+      },
+};
 
 export default withNextIntl(nextConfig);
