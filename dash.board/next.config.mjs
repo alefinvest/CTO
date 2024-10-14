83eb8dc939a7814db9e769225932cb0b01be9b02
Oleksii Bondarenko <71653234+alexmazaltov@users.noreply.github.com>
2024-10-15 02:23:45 +0300

    connect wallet Button added (#3)

--- dash.board/next.config.mjs
+++ dash.board/next.config.mjs
@@ -1,12 +1,16 @@
 import createNextIntlPlugin from 'next-intl/plugin';
- 
+
 const withNextIntl = createNextIntlPlugin("./src/core/i18n/i18n.ts");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
         serverComponentsExternalPackages: ['grammy'],
-      },
+    },
+    env: {
+        TON_CONNECT_PROJECT_ID: process.env.NEXT_PUBLIC_TON_CONNECT_PROJECT_ID,
+        TONCENTER_API_KEY: process.env.TONCENTER_API_KEY,
+    },
 };
 
 export default withNextIntl(nextConfig);
